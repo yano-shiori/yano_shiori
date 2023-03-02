4a52328811f0296d6e490e37d5aa9ee6f8881e6a
YanoShiori <119147388+yano-shiori@users.noreply.github.com>
2023-03-02 16:15:16 +0900

    1.5

--- java/src/Variable1.java
+++ java/src/Variable1.java
@@ -1,6 +1,6 @@
 
 public class Variable1 {
-
+	
 	public static void main(String[] args) {
 		String name = "矢野詩織";
 		char gender = '女';
@@ -14,5 +14,5 @@ public class Variable1 {
 		System.out.println("身長は" + height + "です。");
 		System.out.println("よろしくお願いします。");
 	}
-
+	
 }
