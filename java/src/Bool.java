a515fb25b1d7becafbc13e2cd0229c0f9e6539ef
yano-shiori <kawano.m.office@gmail.com>
2023-03-02 16:08:58 +0900

    1.8

---
+++ java/src/Bool.java
@@ -0,0 +1,25 @@
+public class Bool {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 5;
+		boolean result_1 = a >= b;
+		System.out.println(result_1);
+
+		int c = 5;
+		int d = 10;
+		// 以下更新
+		boolean result_2 = (c != d);
+		System.out.println(result_2);
+
+		int e = 12;
+		int f = 8;
+		boolean result_3 = e > 10 && f < 10;
+		System.out.println(result_3);
+
+		int g = 7;
+		boolean result_4 = g <= 5 || g >= 10;
+		System.out.println(result_4);
+	}
+
+}
