7bd965ea95797d7e3efda75c624f50ddc2694b48
YanoShiori <119147388+yano-shiori@users.noreply.github.com>
2023-03-03 17:19:45 +0900

    1_10_1

--- java/src/Practice.java
+++ java/src/Practice.java
@@ -1,9 +1,11 @@
 public class Practice {
     public static void main(String[] args) {
+        
         int i = 1;
         for (int l = 0; l < 10; l++) {
             i = i + 2;
             System.out.println(i);
+            
         }
     }
 }
