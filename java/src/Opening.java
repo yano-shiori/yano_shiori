88c03279633dcad43252aa51d9441deb410a2129
yano-shiori <kawano.m.office@gmail.com>
2023-03-22 16:48:09 +0900

    1.25_2023/03/22

--- java/src/Opening.java
+++ java/src/Opening.java
@@ -2,6 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Opening {
+    // 1.25
+
     public static void main(String[] args) {
         // キーボード入力
         System.out.println("名前を入力してください");
@@ -25,3 +27,27 @@ public class Opening {
         }
     }
 }
+
+/*
+ *
+ * 名前を入力したら下記がコンソールに出力されるように作ってください
+ * 条件：数値は毎回変わるように作ってください
+ * サブクラスを使用してください
+ * メソッドのオーバーライドを使用してください
+ * superクラスを使用してください
+ * getterとsetterを使用してください
+ * Projectを2つ作ってメインと処理を分けてください
+ * 命名する場合は規則にのっとってください
+ * コンストラクタを使用してください
+ *
+ * こんにちは 「 名前 」 さん
+ * ステータス
+ * HP：849
+ * MP：862
+ * 攻撃力：375
+ * 素早さ：937
+ * 防御力：24
+ *
+ * さあ冒険に出かけよう！
+ *
+ */
